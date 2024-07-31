d7458d45c55f1386c125aaf8f517b999d6e99082
xiaocilebi <107304987+xiaocilebi@users.noreply.github.com>
2024-08-01 00:16:55 +0800

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -13,9 +13,9 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.android.tools)
-    implementation(libs.kotlin.gradle)
-    implementation(libs.eclipse.jgit)
+    implementation("com.android.tools.build:gradle:4.2.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.12.0.202106070339-r")
 }
 
 java {
@@ -26,5 +26,17 @@ java {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "11"
+    }
+}
+
+// 引入公钥进行签名或其他操作
+val publicKey: String? = System.getenv("PUBLIC_KEY")
+if (publicKey != null) {
+    println("Public Key is set")
+    // 在这里您可以添加使用公钥的代码，例如进行文件签名等操作
+    val publicKeyFile = file("$projectDir/app/public_key.pem")
+    if (publicKeyFile.exists()) {
+        println("Public Key File: ${publicKeyFile.readText()}")
+        // 可以在这里使用公钥文件进行进一步的操作
     }
 }
